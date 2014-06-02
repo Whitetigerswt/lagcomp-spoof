3b5cba9fea79eda623857732d375055c9a98427c
Whitetiger <spamsender123@yahoo.com>
2014-06-02 15:55:29 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-lagcomp_spoof
+Lagcomp Mode Spoofer
 =============
 
-Hack client side SA-MP to allow for OnPlayerWeaponShot and GetPlayerLastShotVectors work for servers with lagcomp off.
+Hack client side SA-MP to allow for OnPlayerWeaponShot and GetPlayerLastShotVectors work for servers with lagcomp off, Clients must install this into their game and it cannot be done from server-side.
---
+++ dllmain.cpp
@@ -0,0 +1,29 @@
+#include "memory.h"
+
+void WINAPI Load();
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+					 )
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		if ( CreateThread( 0, 0, (LPTHREAD_START_ROUTINE)Load, NULL, 0, 0) == NULL ) {
+			ExitProcess(GetLastError());
+			return FALSE; 
+		}
+		break;
+	}
+	return TRUE;
+}
+
+void WINAPI Load() {
+	DWORD sampdll = (DWORD)GetModuleHandle("samp.dll");
+	DWORD oldProt = NULL;
+	VirtualProtect((void*)(sampdll + 0x22C799), 6, PAGE_EXECUTE_READWRITE, &oldProt);
+	memcpy((void*)(sampdll + 0x22C799), "\x90\x90\x90\x90\x90\x90", 6);
+	DWORD oldProt2 = NULL;
+	VirtualProtect((void*)(sampdll + 0x22C799), 6, oldProt, &oldProt2);
+}
